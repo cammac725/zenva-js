39e07ea8434ffd6a3f54e986ced2a6962da45974
cammac <camm@rocktechcomputer.com>
2020-07-27 13:58:08 -0700

    added a class

--- ModernJavascript.js
+++ ModernJavascript.js
@@ -3,30 +3,38 @@
 // let apiKey = "30B082CD39F9B6CB9E6AA8548E0A89B9";
 // let appId = "221380"
 
-
-
-
 const fetch = require('node-fetch');
 
-let url =
-  "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=221380";
+let url = "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=221380";
 
-  async function fetchData(url){
-    let response = await fetch(url);
-    let jsonResponse = await response.json();
-    printData(jsonResponse);
+class Achievement {
+  constructor(name, percent) {
+    this.name = name;
+    this.percent = percent;
   }
 
-  function printData(jsonData) {
-    let jsonObject = jsonData["achievementpercentages"];
-    let allAchievements = jsonObject['achievements'];
-
-    for (let achievement of allAchievements){
-      let name = achievement['name'];
-      console.log(name);
-    }
+  printValues() {
+    console.log(`${this.name} achievement has been completed by ${this.percent}% of people.`);
   }
+}
 
-  fetchData(url).catch(function() {
-    console.log("Could not fetch data");
-  });
+
+async function fetchData(url){
+  let response = await fetch(url);
+  let jsonResponse = await response.json();
+  printData(jsonResponse);
+}
+
+function printData(jsonData) {
+  let jsonObject = jsonData["achievementpercentages"];
+  let allAchievements = jsonObject['achievements'];
+
+  for (let achievement of allAchievements){
+    let name = achievement['name'];
+    console.log(name);
+  }
+}
+
+fetchData(url).catch(function() {
+  console.log("Could not fetch data");
+});
